4aace13365701fdcb5e16ddc33d788e141763814
47 <promax47@163.com>
2022-06-30 08:37:20 +0800

    第一次提交

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author ProMax47
+ * @create 2022-06-30-8:35
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
